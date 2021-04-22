7f4dfa48496077f7803e9441d9a54f8d2992315e
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-22 17:02:51 -0300

    feat: create user model and migration

---
+++ lib/banking/user.ex
@@ -0,0 +1,26 @@
+defmodule Banking.User do
+    @moduledoc """
+    The user model.
+    """
+    use Ecto.Schema
+    import Ecto.Changeset
+  
+    @required_fields ~w(email name password balance)a
+    
+    schema "user" do
+      field(:email, :string, unique: true)
+      field(:password, :string)
+      field(:username, :string, unique: true)
+      field(:balance, :float, default: 1.000)
+      
+      has_many(:transfer, Banking.Transfer)
+  
+      timestamps(inserted_at: :created_at)
+    end
+  
+    def changeset(user, attrs) do
+      user
+      |> validate_required(@required_fields)
+      |> unique_constraint(:email)
+    end
+  end
--- lib/banking_web/router.ex
+++ lib/banking_web/router.ex
@@ -1,13 +1,13 @@
 defmodule BankingWeb.Router do
   use BankingWeb, :router
 
-  # pipeline :browser do
-  #   plug :accepts, ["html"]
-  #   plug :fetch_session
-  #   plug :fetch_flash
-  #   plug :protect_from_forgery
-  #   plug :put_secure_browser_headers
-  # end
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
 
   pipeline :api do
     plug :accepts, ["json"]
