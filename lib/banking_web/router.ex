018b502a1090b6708878d37599faffe7ee6bc6e6
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-22 14:01:57 -0300

    feat: add base routes

--- lib/banking_web/router.ex
+++ lib/banking_web/router.ex
@@ -1,28 +1,33 @@
 defmodule BankingWeb.Router do
   use BankingWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+  # pipeline :browser do
+  #   plug :accepts, ["html"]
+  #   plug :fetch_session
+  #   plug :fetch_flash
+  #   plug :protect_from_forgery
+  #   plug :put_secure_browser_headers
+  # end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", BankingWeb do
-    pipe_through :browser
+  # scope "/", BankingWeb do
+  #   pipe_through :browser
 
-    get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", BankingWeb do
-  #   pipe_through :api
+  #   get "/", PageController, :index
   # end
+
+  scope "/api", BankingWeb do
+    pipe_through :api
+     
+    post("/accounts", AccountController, :create) 
+    post("/signin", SessionController, :create)
+    get("/transfers", TransfersController, :index)
+    post("/transfers", TransfersController, :create)
+    post("/withdraw", WithdrawController, :create)
+  end
 
   # Enables LiveDashboard only for development
   #
