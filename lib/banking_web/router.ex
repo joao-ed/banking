a7b334be7f3c1e8a82fe2029ecca810933e9dbb1
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-23 12:47:08 -0300

    refactor: remove unnecessary code from router.ex

--- lib/banking_web/router.ex
+++ lib/banking_web/router.ex
@@ -1,47 +1,22 @@
 defmodule BankingWeb.Router do
   use BankingWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
+    # plug :fetch_session
+    # plug :fetch_flash
+    # plug :protect_from_forgery
+    # plug :put_secure_browser_headers
   end
 
-  # scope "/", BankingWeb do
-  #   pipe_through :browser
-
-  #   get "/", PageController, :index
-  # end
 
   scope "/api", BankingWeb do
     pipe_through :api
-     
-    post("/accounts", AccountController, :create) 
+
+    post("/accounts", AccountController, :create)
     post("/signin", SessionController, :create)
-    get("/transfers", TransfersController, :index)
-    post("/transfers", TransfersController, :create)
+    resources("/transfers", TransfersController, only: [:index, :create])
     post("/withdraw", WithdrawController, :create)
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: BankingWeb.Telemetry
-    end
-  end
 end
