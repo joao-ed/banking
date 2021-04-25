1b5bf4d130a8bbaccddcad8d9964e4f033adbdb1
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-25 19:25:51 -0300

    feat: implementing guardian module

---
+++ lib/banking_web/guardian.ex
@@ -0,0 +1,11 @@
+defmodule BankingWeb.Guardian do
+  use Guardian, otp_app: :banking
+
+  alias Banking.{Repo, User}
+
+  def subject_for_token(%User{} = user, _claims), do: {:ok, to_string(user.id)}
+  def subject_for_token(_, _), do: {:error, "Unknown resource type"}
+
+  def resource_from_claims(%{"sub" => user_id}), do: {:ok, Repo.get(User, user_id)}
+  def resource_from_claims(_claims), do: {:error, "Unknown resource type"}
+end
