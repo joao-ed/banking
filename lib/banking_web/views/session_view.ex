d3acfb733640e2d153a6b8d2b373f8fcc7049c24
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-25 19:28:14 -0300

    feat: update SessionView

---
+++ lib/banking_web/views/session_view.ex
@@ -0,0 +1,11 @@
+defmodule BankingWeb.SessionView do
+  use BankingWeb, :view
+
+  def render("error.json", %{message: message}) do
+    %{message: message}
+  end
+
+  def render("login.json", %{jwt: jwt}) do
+    %{token: jwt}
+  end
+end
