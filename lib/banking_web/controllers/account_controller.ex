1c5fdda29877b7f55beb20a4b8388bc5cdc5a9d7
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-25 16:41:25 -0300

    feat: add fallback controller to AccountController

--- lib/banking_web/controllers/account_controller.ex
+++ lib/banking_web/controllers/account_controller.ex
@@ -2,6 +2,8 @@ defmodule BankingWeb.AccountController do
   use BankingWeb, :controller
   alias Banking.Auth
 
+  action_fallback(BankingWeb.FallbackController)
+
   def create(conn, params) do
     case Auth.register(params) do
       {:ok, user} ->
@@ -9,7 +11,7 @@ defmodule BankingWeb.AccountController do
         |> put_status(:created)
         |> text("User created!")
 
-      {:error, changeset} -> 
+      {:error, changeset} ->
         conn
         |> put_status(:bad_request)
         |> render(BankingWeb.ChangesetView, "error.json", changeset: changeset)
