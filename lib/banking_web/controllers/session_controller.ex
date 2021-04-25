d75a65f21cb08b465ef40ab59e367264e8a5698e
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-25 16:42:29 -0300

    feat: implementing SessionController

--- lib/banking_web/controllers/session_controller.ex
+++ lib/banking_web/controllers/session_controller.ex
@@ -1,9 +1,18 @@
 defmodule BankingWeb.SessionController do
   use BankingWeb, :controller
+  alias Banking.Auth
 
-  def index(conn, params) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(204, "")
+  action_fallback(BankingWeb.FallbackController)
+
+  def create(conn, params) do
+    case Auth.find_user_and_check_password(params) do
+      {:ok, user} ->
+        json(conn, %{ok: "authenticated!"})
+
+      {:error, message} ->
+        conn
+        |> put_status(:unauthorized)
+        |> render(BankingWeb.UserView, "error.json", message: message)
+    end
   end
 end
