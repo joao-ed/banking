3ac6405db2904cf407857a71fd4a7a7dd2fb24f6
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-27 13:07:08 -0300

    chore: improve docker config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 # Docker entrypoint script.
 
 # Wait until Postgres is ready
-while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+while ! pg_isready -q -h $HOSTNAME -p $PORT -U $USER
 do
   echo "$(date) - waiting for database to start"
   sleep 2
 done
 
 # Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
-  echo "Database $PGDATABASE does not exist. Creating..."
-  createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
+if [[ -z `psql -Atqc "\\list $DATABASE"` ]]; then
+  echo "Database $DATABASE does not exist. Creating..."
+  createdb -E UTF8 $DATABASE -l en_US.UTF-8 -T template0
   mix ecto.setup
-  echo "Database $PGDATABASE created."
+  echo "Database $DATABASE created."
 fi
 
 exec mix phx.server
