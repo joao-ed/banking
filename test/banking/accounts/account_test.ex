d37faeea80028956d093185f89aa6050760b9cdf
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-05-03 21:26:48 -0300

    chore: add account test

---
+++ test/banking/accounts/account_test.ex
@@ -0,0 +1,9 @@
+defmodule Banking.Accounts.AccountTest do
+  use Banking.DataCase, async: true
+  alias Banking.Accounts.Account
+
+  test "balance must be equal or greater than zero" do
+    changeset = Account.changeset(%Account{}, %{balance: 1})
+    assert %{balance: ["Insufficient funds to perform this operation"]} = errors_on(changeset)
+  end
+end
